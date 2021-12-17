184ae238f136394af9e7ec02b62f9d8bd25e3039
Raul Hernandez <rjhb1985@gmail.com>
2021-12-17 14:38:20 +0100

    modification initiation.java

--- src/initiation/Initiation.java
+++ src/initiation/Initiation.java
@@ -14,7 +14,7 @@ public class Initiation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD Rulls");
+        System.out.println("HELLO WORLD RULLS");
     }
     
 }
